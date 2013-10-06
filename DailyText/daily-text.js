4698e9063ce7c0c26803b740367e9bc889c9b039
KennyBu <ken.burkhardt@gmail.com>
2013-10-06 13:58:37 -0400

    Added Chrome Extension

--- DailyText/daily-text.js
+++ DailyText/daily-text.js
@@ -1,3 +1,14 @@
 $(function () {
-    $('#date-line').text('ken test');
+    //$('#header-text').text('Daily Text');
+
+    $.getJSON("http://assignit.apphb.com/DailyText/Json", null, function (result) {
+        var textDate = $('#text-date');
+        var textheader = $('#text-header');
+        var textBody = $('#text-body');
+
+        textDate.append(result.DateLine);
+        textheader.append("<i>" + result.Header + "</i>");
+        textBody.append(result.Body);
+
+    });
 });
