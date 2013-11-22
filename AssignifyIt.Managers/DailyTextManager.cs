648088f7ec202f124ce8d5aaf03977139c9c7417
Ken Burkhardt <ken.burkhardt@gmail.com>
2013-11-22 09:16:15 -0500

    Added  Logentries support with nLog

--- AssignifyIt.Managers/DailyTextManager.cs
+++ AssignifyIt.Managers/DailyTextManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using AssignifyIt.Models;
 using AssignifyIt.Queries.DailyTexts;
 using HtmlAgilityPack;
+using NLog;
 
 namespace AssignifyIt.Managers
 {
@@ -17,6 +18,7 @@ namespace AssignifyIt.Managers
     {
         private readonly IDailyTextManagerQuery _dailyTextManagerQuery;
         private readonly IRedisManager _redisManager;
+        private static Logger logger = LogManager.GetCurrentClassLogger();
 
         public DailyTextManager(IDailyTextManagerQuery dailyTextManagerQuery, IRedisManager redisManager)
         {
@@ -31,21 +33,26 @@ namespace AssignifyIt.Managers
 
             //First Check Redis Cache
             var dailyText = _redisManager.GetDailyText(easternTime);
-            
-            if(dailyText != null)
+
+            if (dailyText != null)
+            {
+                logger.Info(string.Format("Daily Text Found in Redis with date: {0}", easternTime));
                 return dailyText;
+            }
+                
             
             //Next Check SQL Server
             dailyText = _dailyTextManagerQuery.GetDailyText(easternTime);
             if (dailyText != null)
             {
+                logger.Info(string.Format("Daily Text Found in SQL Server with date: {0}", easternTime));
                 _redisManager.InsertDailyText(dailyText);
                 return dailyText;
             }
             
             //Retrieve the text from the online library
             var url = string.Format("http://wol.jw.org/en/wol/dt/r1/lp-e/{0}/{1}/{2}", easternTime.Year,easternTime.Month,easternTime.Day);
-        var web = new HtmlWeb();
+            var web = new HtmlWeb();
             var doc = web.Load(url);
 
             dailyText = new DailyText
@@ -57,6 +64,7 @@ namespace AssignifyIt.Managers
                     DateEntered = easternTime
                 };
 
+            logger.Info(string.Format("Daily Text retrieved from WOL with date: {0}", easternTime));
             //Put the text into the cache & SQL Server
             _redisManager.InsertDailyText(dailyText);
             _dailyTextManagerQuery.InsertDailyText(dailyText);
